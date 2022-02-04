3ecce14c191381b163705c8f0ffd1fdfbcd86486
Ryan Monson <monson.r@gmail.com>
2022-02-03 21:00:58 -0700

    Updated to slower but hopefully more accurate solver

--- analyze_efficiency.py
+++ analyze_efficiency.py
@@ -1,5 +1,53 @@
-import logging
+import time
 import solve_wordle
+from multiprocessing import Pool
 
-all_words = solve_wordle.load_words('wordle_words.json')
-print(solve_wordle.find_steps_required_to_solve('apple', all_words))
+
+first = [(1, 1),
+(2, 178),
+(3, 2060),
+(4, 4429),
+(5, 3170),
+(6, 1589),
+(7, 753),
+(8, 399),
+(9, 201),
+(10, 100),
+(11, 51),
+(12, 24),
+(13, 10),
+(14, 3),
+(15, 3),
+(16, 1)]
+
+# (14, ['bares', 'cills', 'rarks'])
+# (15, ['bills', 'karks', 'rares'])
+# (16, ['lills'])
+
+def find_parallel(all_words, words_to_find):
+    words_by_count = {}
+    guesses_to_show = 0
+    debug = False
+    data = [(word, all_words, guesses_to_show, debug) for word in words_to_find]
+    with Pool(14) as p:
+        results = p.map(solve_wordle.find_steps_required_to_solve, data)
+        for res in results:
+            counter, word = res
+            if counter not in words_by_count:
+                words_by_count[counter] = []
+            words_by_count[counter].append(word)
+
+    print()
+    for counter in sorted(words_by_count.keys()):
+        print((counter, len(words_by_count[counter])))
+        if counter > 13:
+            print((counter, words_by_count[counter]))
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    all_words = solve_wordle.load_words('wordle_words.json')
+    find_parallel(all_words, all_words)
+    # counter, word = solve_wordle.find_steps_required_to_solve(('karks', all_words, 0, False))
+    # print(f"It took {counter} guesses to get {word}")
+    print(f"Elapsed time {time.time() - t0: 0.2f} seconds")
--- solve_wordle.py
+++ solve_wordle.py
@@ -1,6 +1,7 @@
 import json
 import string
 from typing import Tuple, Dict, List, Set, Optional
+import logging
 
 def get_words_with_letters(words: Set[str], 
                            required_letters: List[Tuple[chr, Optional[int]]], 
@@ -36,23 +37,33 @@ def get_words_with_letters(words: Set[str],
 
 def get_words_by_letter(words):
     words_by_letter = {}
+    for letter in string.ascii_lowercase:
+        words_by_letter[letter] = set()
     for word in words:
         for letter in word:
-            if letter not in words_by_letter:
-                words_by_letter[letter] = set()
             words_by_letter[letter].add(word)
     return words_by_letter
 
-def get_most_unique_score(words: Set[str]) -> List[Tuple[int, str]]:
+def get_most_unique_score_first(allowed_words: Set[str], all_words: Set[str]) -> List[Tuple[int, str]]:
     scores_and_words = []
-    words_by_letter = get_words_by_letter(words)
-    for word in words:
+    words_by_letter = get_words_by_letter(allowed_words)
+    num_allowed_words = len(allowed_words)
+    for letter in words_by_letter:
+        if len(words_by_letter[letter]) == num_allowed_words:
+            words_by_letter[letter] = set()
+    for word in all_words:
         score = 0
+        if word in allowed_words:
+            score += 0.1
+        tmp = set()
         for letter in set(word):
-            score += len(words_by_letter[letter])
+            tmp = tmp.union(words_by_letter[letter])
+        score += len(tmp)
         scores_and_words.append((score, word))
     best = sorted(scores_and_words, reverse=True)
     return best
+
+get_most_unique_score = get_most_unique_score_first
 
 def simulate_guess(guess, word):
     response = '1'
@@ -65,27 +76,26 @@ def simulate_guess(guess, word):
             response += 'n'
     return response
 
-def single_loop(words: Set[str], 
+def single_loop(all_words: Set[str], 
                 required_letters: Set[Tuple[chr, Optional[int]]], 
                 exclude_letters: Set[Tuple[chr, Optional[int]]],
                 guesses_to_show=3, guess_to_simulate=None):
-    allowed_words, not_allowed_words = get_words_with_letters(words, 
+    allowed_words, not_allowed_words = get_words_with_letters(all_words, 
                            required_letters, 
                            exclude_letters)
-
-    guesses = get_most_unique_score(allowed_words)
+    guesses = get_most_unique_score(allowed_words, all_words)
     if guesses_to_show > 0:
         print(f"\nNumber of Words Remaining: {len(allowed_words)}")
         for i in range(min(guesses_to_show, len(guesses))):
             print(f"{i + 1}. {guesses[i][1]} with score of {guesses[i][0]}")
-    if simulate_guess is None:
+    if guess_to_simulate is None:
         response = input("Enter your pick and the results: ")
     else:
         response = simulate_guess(guesses[0][1], guess_to_simulate)
     pick_value = int(response[0]) - 1
     if pick_value == -1:
         print("Exiting")
-        return (None, None, allowed_words)
+        return (None, None, allowed_words, None)
     word = guesses[pick_value][1]
     new_required_letters = required_letters.copy()
     new_exclude_letters = exclude_letters.copy()
@@ -107,29 +117,34 @@ def single_loop(words: Set[str],
 def run_all(words):
     required_letters = set()
     exclude_letters = set()
-    allowed_words = words.copy()
+    all_words = words.copy()
     while required_letters is not None:
         # print(("required", required_letters))
         # print(("excluded", exclude_letters))
-        required_letters, exclude_letters, allowed_words, guess = single_loop(allowed_words, 
+        required_letters, exclude_letters, allowed_words, guess = single_loop(all_words, 
                                                                        required_letters, 
-                                                                       exclude_letters)
+                                                                       exclude_letters, 
+                                                                       guesses_to_show=5,
+                                                                       guess_to_simulate=None)
 
-def find_steps_required_to_solve(word_to_find, words, guesses_to_show=0):
+def find_steps_required_to_solve(args):
+    (word_to_find, words, guesses_to_show, debug) = args
     counter = 0
     required_letters = set()
     exclude_letters = set()
-    allowed_words = words.copy()
+    all_words = words.copy()
     while required_letters is not None:
-        required_letters, exclude_letters, allowed_words, guess = single_loop(allowed_words, 
+        required_letters, exclude_letters, allowed_words, guess = single_loop(all_words, 
                                                                        required_letters, 
                                                                        exclude_letters, 
                                                                        guesses_to_show=guesses_to_show,
                                                                        guess_to_simulate=word_to_find)
+        if debug:
+            print(f'guess = {guess}')
         counter += 1
         if guess == word_to_find:
             break
-    return counter
+    return counter, word_to_find
 
 def load_words(file_name):
     with open(file_name, 'rt') as f:
